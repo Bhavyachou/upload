45916c960b3c90a9fcd6507f002f49e173676145
Bhavyachou <51315468+Bhavyachou@users.noreply.github.com>
2019-06-26 16:21:55 +0530

    Updated upload backend

--- back-end/index.js
+++ back-end/index.js
@@ -1,37 +1,38 @@
 const express = require("express");
 const multer = require("multer");
-const path = require("path");
-const fs = require("fs");
 const mysql = require("mysql");
 const cors = require("cors");
 var bodyParser = require("body-parser");
 
-
+const upload = multer({dest: "./public/uploads"});
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-const db = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'data1'
-});
 
-
-const query = sqlStatement =>
-  new Promise((resolve, reject) => {
-    db.query(sqlStatement, (err, results) => {
-      if (err) return reject(err);
-      resolve(results);
-    });
-  });
   //const db1 = mysql.createConnection({
     //host     : '34.205.83.88',
     //user     : 'analytics_proto',
     //password : 'analytics_123',
     //database : 'Zolo_Centers'
   //});
+  const db = mysql.createConnection({
+    host     : '34.205.83.88',
+    user     : 'analytics_proto',
+    password : 'analytics_123',
+    database : 'Analytics_Prototype'
+  });
+  
+  const db1 = mysql.createConnection({
+    host     : '34.205.83.88',
+    user     : 'analytics_proto',
+    password : 'analytics_123',
+    database : 'Zolo_Centers'
+  });
+  
+  const staticFileMiddleware = express.static("dist");
 
+  // 1st call for unredirected requests
+  app.use(staticFileMiddleware);
   
 app.use(
   cors({
@@ -51,44 +52,22 @@ app.get("/", (req, res) => {
 
 //};
 
+const query = sqlStatement =>
+  new Promise((resolve, reject) => {
+    db.query(sqlStatement, (err, results) => {
+      if (err) return reject(err);
+      resolve(results);
+    });
+  });
 
-
-const upload = multer({
-  dest: "./public/uploads"
-  // you might also want to set some limits: https://github.com/expressjs/multer#limits
-});
-
-
-app.post(
-  "/upload",
-  upload.single("file" /* name attribute of <file> element in your form */),
-  (req, res) => {
-    const tempPath = req.file.path;
-    const targetPath = path.join(__dirname, "./uploads/image.png");
-
-    if (path.extname(req.file.originalname).toLowerCase() === ".png") {
-      fs.rename(tempPath, targetPath, err => {
-        if (err) return handleError(err, res);
-
-        res
-          .status(200)
-          .contentType("text/plain")
-          .end("File uploaded!");
-      });
-    } else {
-      fs.unlink(tempPath, err => {
-        if (err) return handleError(err, res);
-
-        res
-          .status(403)
-          .contentType("text/plain")
-          .end("Only .png files are allowed!");
-      });
-    }
-    console.log("hi");
-  }
-);
-
+  const query1 = sqlStatement =>
+  new Promise((resolve, reject) => {
+    db1.query(sqlStatement, (err, results) => {
+      if (err) return reject(err);
+      resolve(results);
+    });
+  });
+   
 /**
  * this mehtod insertes a new table record filling the ID automatically meaning no need to send the ID with the request
  * @param {string} tableName
@@ -112,7 +91,19 @@ const insertTableRow = tableName => async (req, res) => {
 
 
 
-
+app.post('/api/upload/', upload.single('file'), (req, res) => {
+  console.log("Going inside");
+  if(req.file) {
+      res.json(req.file);
+  }
+  else throw 'error';
+});
+const getcityname = tableName => async (req, res) => { 
+  console.log('Hey there')
+  const city = await query1( `select DISTINCT CITY, LOCALNAME from ${tableName} WHERE APPROVED = 1 AND TYPE = 'PROPERTY' `  );
+  console.log('city',city);
+  res.json(city);
+}; 
 /**
  * this method creates the basic API template for a given table name using the name as route of the api
  * @param {string} tableName
@@ -120,12 +111,12 @@ const insertTableRow = tableName => async (req, res) => {
 
 
 const createTableBasicAPI = tableName => {
- // app.get(`/${tableName}/getcittyname`, getcityname(`${tableName}`));
+ app.get(`/${tableName}/getcittyname`, getcityname(`${tableName}`));
   app.post(`/${tableName}/insert`, insertTableRow(`${tableName}`));
 };
 
 createTableBasicAPI("upload");
-//createTableBasicAPI("Centers");
+createTableBasicAPI("Centers");
 const PORT = process.env.PORT || 3000;
 console.log('server running at', PORT);
 app.listen(PORT);
